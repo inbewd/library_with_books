55f1507248cc83ca5cf8ccbf10497b1848e48cbb
hmd-git <hmd.node@gmail.com>
2014-11-05 20:15:12 -0500

    added connect.rb NOT in the model directory. Sorta messed up the commit a bit because I added it to that directory first, let's see how it looks

--- main.rb
+++ main.rb
@@ -1,16 +1,8 @@
 
-require 'active_record'
+
 require 'pry'
 
 require_relative 'models'
-
-ActiveRecord::Base.establish_connection({
-  adapter: "sqlite3",
-  database: "./library.db"
-
-  })
-
-puts "Connected!"
-
+require_relative 'connect'
 
 binding.pry
