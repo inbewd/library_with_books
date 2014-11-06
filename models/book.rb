f098fa93980f99832ce38a0e86dbbb59dd842104
hmd-git <hmd.node@gmail.com>
2014-11-05 20:53:32 -0500

    experiments with validate in model/book.rb

--- models/book.rb
+++ models/book.rb
@@ -1,5 +1,9 @@
 require 'active_record'
 
 class Book < ActiveRecord::Base
-  
+  validates :title, 
+            presence: true
+
+  validates :publication_date, 
+            numericality: {only_integer: true}
 end
